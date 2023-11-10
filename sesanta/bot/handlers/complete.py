4d8ca38a4c0b9ebd1af3577ae8875716dcc4523b
Oskar Sharipov <oskargit@riseup.net>
2023-11-10 21:59:08 +0700

    Show selected_countries in /complete

--- sesanta/bot/handlers/complete.py
+++ sesanta/bot/handlers/complete.py
@@ -4,7 +4,7 @@ from aiogram.types import Message
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from sesanta.bot.filters import IsEligibleFilter
-from sesanta.services.country_chooser import CountryChooser
+from sesanta.services.country_chooser import CountryChooser, hash_country
 from sesanta.services.user_getter import UserGetter
 from sesanta.services.user_set_completeness import UserCompletenessSetter
 
@@ -26,9 +26,12 @@ async def handler(
         )
         return
     await UserCompletenessSetter(db)(message.chat.id, is_complete=True)
+    selected = ", ".join(sorted(user.selected_countries, key=hash_country))
+    # selected is sorted in a weird order FOR PURPOSE
     await message.answer(
         "Сохранили твою анкету. "
         "Если захочешь внести изменения, прожми /start.\n\n"
+        f"Выбранные страны: {selected}.\n\n"
         "Можно указать свой адрес и полное имя сейчас: "
         "для этого напиши их одним сообщением. "
         "Твой санта укажет эти данные в полях получателя.\n\n"
