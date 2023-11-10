f7e712817cf344ed0a15502b062a58fdaea10304
Oskar Sharipov <oskargit@riseup.net>
2023-11-10 22:04:48 +0700

    I'm sorry

--- sesanta/bot/handlers/delete.py
+++ sesanta/bot/handlers/delete.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
@@ -14,4 +16,6 @@ async def handler(
     db: AsyncIOMotorDatabase,
 ) -> None:
     await UserDeleter(db)(message.chat.id)
-    await message.answer("Мы знакомы?")
+    await message.answer("Аккаунт в клубе удален.")  # I'm sorry
+    await asyncio.sleep(5)
+    await message.answer("Шутка.")
