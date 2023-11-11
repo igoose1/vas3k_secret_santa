a0cb09f3826dbd9d73caefe66eeadff8374d5bbc
Oskar Sharipov <oskargit@riseup.net>
2023-11-11 18:39:59 +0700

    Add confirm_handler for /delete

--- sesanta/bot/handlers/delete.py
+++ sesanta/bot/handlers/delete.py
@@ -2,20 +2,47 @@ import asyncio
 
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import CallbackQuery, Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from sesanta.services.user_deleter import UserDeleter
 
 router = Router()
 
+CONFIRM_BUTTON_TEXT = "Да, точно удалить"
+
+
+class ConfirmDeletionCallback(CallbackData, prefix="condel"):
+    ...
+
 
 @router.message(Command("delete"))
 async def handler(
     message: Message,
+) -> None:
+    keyboard = InlineKeyboardBuilder()
+    keyboard.button(
+        text=CONFIRM_BUTTON_TEXT,
+        callback_data=ConfirmDeletionCallback(),
+    )
+    await message.answer(
+        "Удалить аккаунт? Все данные сотрутся и их нельзя будет вернуть.",
+        reply_markup=keyboard.as_markup(),
+    )
+
+
+@router.callback_query(ConfirmDeletionCallback.filter())
+async def confirm_handler(
+    callback_query: CallbackQuery,
+    callback_data: ConfirmDeletionCallback,
     db: AsyncIOMotorDatabase,
 ) -> None:
-    await UserDeleter(db)(message.chat.id)
-    await message.answer("Аккаунт в клубе удален.")  # I'm sorry
-    await asyncio.sleep(5)
-    await message.answer("Шутка.")
+    await UserDeleter(db)(callback_query.from_user.id)
+    if callback_query.message is None:
+        await callback_query.answer("Удалено", show_alert=True)
+    else:
+        await callback_query.message.answer("Аккаунт в клубе удален.")  # I'm sorry
+        await asyncio.sleep(5)
+        await callback_query.message.answer("Шутка")
