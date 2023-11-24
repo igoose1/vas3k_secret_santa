3fcf6a3e161b39f0672e6036002c29feef9c5110
Oskar Sharipov <oskargit@riseup.net>
2023-11-24 12:11:59 +0700

    Add "not-completed-only" option to export_users.py

--- utils/export_users.py
+++ utils/export_users.py
@@ -14,8 +14,13 @@ UserListAdapter = TypeAdapter(list[User])
 
 @click.command()
 @click.option("--completed-only", default=False, help="Only completed")
+@click.option("--not-completed-only", default=False, help="Only completed")
 @click.option("--no-address-only", default=False, help="Only without address")
-async def main(completed_only: bool, no_address_only: bool) -> None:
+async def main(
+    completed_only: bool,
+    not_completed_only: bool,
+    no_address_only: bool,
+) -> None:
     """Export users to stdout."""
     db = AsyncIOMotorClient(str(settings.mongo_uri))[settings.mongo_db]
     collection = UserCollection(db)
@@ -25,6 +30,8 @@ async def main(completed_only: bool, no_address_only: bool) -> None:
     filter_ = {}
     if completed_only:
         filter_.update({"is_completed": True})
+    if not_completed_only:
+        filter_.update({"is_completed": False})
     if no_address_only:
         filter_.update({"address": None})
     async for user in collection.get_all(filter_):
