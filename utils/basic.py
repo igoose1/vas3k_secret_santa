fc4f7931561bbb0edcf0729cf0c8f6e7865699f7
Oskar Sharipov <oskargit@riseup.net>
2023-11-24 12:09:10 +0700

    Add "--no-address-only" option for export_users.py

--- utils/basic.py
+++ utils/basic.py
@@ -4,6 +4,7 @@ import pydantic
 class User(pydantic.BaseModel):
     slug: str
     telegram_id: int
+    address: str | None
     location: str | None
     selected: list[str]
     is_completed: bool
--- utils/export_users.py
+++ utils/export_users.py
@@ -14,18 +14,25 @@ UserListAdapter = TypeAdapter(list[User])
 
 @click.command()
 @click.option("--completed-only", default=False, help="Only completed")
-async def main(completed_only: bool) -> None:
+@click.option("--no-address-only", default=False, help="Only without address")
+async def main(completed_only: bool, no_address_only: bool) -> None:
     """Export users to stdout."""
     db = AsyncIOMotorClient(str(settings.mongo_uri))[settings.mongo_db]
     collection = UserCollection(db)
 
     result: list[User] = []
     filter_ = {"is_completed": True} if completed_only else {}
+    filter_ = {}
+    if completed_only:
+        filter_.update({"is_completed": True})
+    if no_address_only:
+        filter_.update({"address": None})
     async for user in collection.get_all(filter_):
         result.append(
             User(
                 slug=user.slug,
                 telegram_id=user.telegram_id,
+                address=user.address,
                 location=user.location,
                 selected=list(user.selected_countries),
                 is_completed=user.is_completed,
