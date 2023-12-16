c26bd50c919dbe54293e5e2b0f0e52e517cd3a38
Oskar Sharipov <oskargit@riseup.net>
2023-12-16 22:17:20 +0700

    Simplify utils/export_users.py
    Use jq instead:
    
    	python -m utils.export_users \
    		| hjson -j \
    		| jq 'map(select(.is_completed == true))'

--- utils/export_users.py
+++ utils/export_users.py
@@ -13,28 +13,13 @@ UserListAdapter = TypeAdapter(list[User])
 
 
 @click.command()
-@click.option("--completed-only", default=False, help="Only completed")
-@click.option("--not-completed-only", default=False, help="Only completed")
-@click.option("--no-address-only", default=False, help="Only without address")
-async def main(
-    completed_only: bool,
-    not_completed_only: bool,
-    no_address_only: bool,
-) -> None:
+async def main() -> None:
     """Export users to stdout."""
     db = AsyncIOMotorClient(str(settings.mongo_uri))[settings.mongo_db]
     collection = UserCollection(db)
 
     result: list[User] = []
-    filter_ = {"is_completed": True} if completed_only else {}
-    filter_ = {}
-    if completed_only:
-        filter_.update({"is_completed": True})
-    if not_completed_only:
-        filter_.update({"is_completed": False})
-    if no_address_only:
-        filter_.update({"address": None})
-    async for user in collection.get_all(filter_):
+    async for user in collection.get_all({}):
         result.append(
             User(
                 slug=user.slug,
